1de320be991e1d42165a4df82b95dc5fc09a9fcc
silentunion <milliner@unbc.ca>
2020-12-22 11:14:20 -0800

    Converted insert parts to a class, added future ability to add full alphabet on demand

--- package/insertions/insert_parts.py
+++ package/insertions/insert_parts.py
@@ -2,18 +2,22 @@ from ..database.database import LORConnect
 
 db = LORConnect()
 
-v, c = 'vowel', 'consonant'
+class Insertions():
+    def insert_alphabet(self):
+        v, c = 'vowel', 'consonant'
 
-parts = ['i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-category = 'letters'
-collection = 'English Basic'
-properties = [v, c, c, c, c, c, v, c, c, c, c, c, v, c, c, c, v, c]
+        parts = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        category = 'letters'
+        collection = 'English Basic'
+        properties = [v, c, c, c, v, c, c, c, v, c, c, c, c, c, v, c, c, c, c, c, v, c, c, c, v, c]
 
-if len(parts) == len(properties):
-    for p in range(0, len(parts)):
-        db.insert_part(parts[p], category)
-        db.add_part_to_collection(parts[p], category, collection)
-        db.add_prop_to_part(parts[p], category, collection, properties[p])
+        if len(parts) == len(properties):
+            for p in range(0, len(parts)):
+                db.insert_part(parts[p], category)
+                db.add_part_to_collection(parts[p], category, collection)
+                db.add_prop_to_part(parts[p], category, collection, properties[p])
 
-else:
-    print('List lengths do not match')
+        else:
+            print('List lengths do not match')
+    
