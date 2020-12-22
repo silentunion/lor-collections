700f9284bc86721aec0721fee9708184123a180d
silentunion <milliner@unbc.ca>
2020-12-22 09:36:34 -0800

    Added function to get col_id and part_id if exists

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from package.database.database import LORConnect
 
 def run():
     db = LORConnect()
+    print(db.get_col_part_if_exists('i', 'letters', 'English Basic'))
     db.get_parts()
 
 if __name__ == "__main__":
--- package/database/database.py
+++ package/database/database.py
@@ -59,3 +59,48 @@ class LORConnect():
                 self.conn.close()
 
         self.disconnect()
+
+    def get_col_part_if_exists(self, part, category, collection):
+        self.connect()
+
+        col_part_if_exists = """SELECT part_id, col_id FROM namegen.collection_parts
+                                JOIN namegen.collections USING(col_id)
+                                JOIN namegen.parts USING(part_id)
+                                WHERE part = %s
+                                  AND category = %s
+                                  AND collection = %s;"""
+       
+        self.cur.execute(col_part_if_exists, (part, category, collection,))
+        
+        result = self.cur.fetchone()
+        self.disconnect()
+        return result
+
+    def add_part_to_collection(self, part, category, collection):
+        self.connect()
+
+        check_if_exists = """SELECT part_id, col_id FROM namegen.collection_parts
+                             """
+
+        add_part_to_col = """INSERT INTO namegen.parts (col_id, part_id)
+                              SELECT %s, %s
+                              WHERE NOT EXISTS (
+                                  SELECT 1 FROM namegen.collection_parts
+                                  JOIN 
+                                                WHERE part = %s
+                                                  AND category = %s);"""
+       
+        try:
+            self.cur.execute(insert_part, (part, category, part, category,))
+            self.conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            sql_file_path = base_path / 'sql' / 'maintenance' / 'serial_resets.sql'
+            with open(sql_file_path) as sql_file:
+                sql_as_string = sql_file.read()
+                self.cur.executescript(sql_as_string)
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+
+        self.disconnect()
