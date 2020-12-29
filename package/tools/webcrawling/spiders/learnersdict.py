880273486f75e60ddaed0694b7ae87ce850ff99e
silentunion <milliner@unbc.ca>
2020-12-29 08:34:29 -0800

    dict spider correctly grabs the words from 1 page

--- package/tools/webcrawling/spiders/learnersdict.py
+++ package/tools/webcrawling/spiders/learnersdict.py
@@ -4,8 +4,17 @@ import scrapy
 
 class LearnersdictSpider(scrapy.Spider):
     name = 'learnersdict'
-    allowed_domains = ['learnersdictionary.com']
-    start_urls = ['http://learnersdictionary.com/']
+    allowed_domains = ['www.learnersdictionary.com']
+    start_urls = ['https://learnersdictionary.com/3000-words/alpha/a']
 
     def parse(self, response):
-        pass
+        items = response.xpath('//*[contains(@class,"words")]/li/a/text()').extract()
+
+        for item in items:
+            word = item.strip()
+
+            if len(word) > 0:
+                word = word.split(' ', 1)[0]
+                yield {"word": word}
+            
+
