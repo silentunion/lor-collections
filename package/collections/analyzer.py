10b3ba2c0d87f3f179b4d4a37a358084a2d4caca
silentunion <milliner@unbc.ca>
2020-12-29 13:47:35 -0800

    Moved the print statements to a def, fancied the statements

--- package/collections/analyzer.py
+++ package/collections/analyzer.py
@@ -29,45 +29,31 @@ def add_item_to_dic(item, dic):
     else:
         dic[item] = 1
 
-# Converts values added up to a percentile
-def convert_to_percent(total, dic):
-    for d in dic:
-        total += dic[d]
-    for d in dic:
-        dic[d] = round(dic[d]/total*100, 5)
+
 
 
 for word in words_list:
     for l in range(len(word)):
         if word[l] in vowels_list:
             letter_type = 'vowel'
-
-            # ADD TO VOWELS
             add_item_to_dic(word[l], vowels)
  
         elif word[l] in consonants_list:
             letter_type = 'consonant'
-
-            #ADD TO CONSONANTS
             add_item_to_dic(word[l], cons)
 
         else:
             letter_type = 'other'
 
-        # ADD TO LETTERS
         add_item_to_dic(word[l], letters)
         
         if l != 0:
             if letter_type == 'consonant' and prev_letter_type == 'consonant':
                 cl = prev_letter + word[l]
-
-                # ADD TO CONS CLUSTERS
                 add_item_to_dic(cl, cons_clusters)
             
             if letter_type == 'vowel' and prev_letter_type == 'vowel':
                 cl = prev_letter + word[l]
-                
-                # ADD TO VOWEL CLUSTERS
                 add_item_to_dic(cl, vowel_clusters)
 
         prev_letter = word[l]
@@ -76,19 +62,31 @@ for word in words_list:
 #-------------------------------------#
 # CONVERSION OF TOTALS TO FREQUENCIES #
 #-------------------------------------#
+# Converts values added up to a percentile
+def convert_to_percent(total, dic):
+    for d in dic:
+        total += dic[d]
+    for d in dic:
+        dic[d] = round(dic[d]/total*100, 5)
+
 convert_to_percent(total_letters, letters)
 convert_to_percent(total_cons, cons)
 convert_to_percent(total_vowels, vowels)
 convert_to_percent(total_cons_cl, cons_clusters)
 convert_to_percent(total_vowels_cl, vowel_clusters)
 
+def print_result(title, dic):
+    ruler = ''
+    for r in range(len(title)):
+        ruler += '='
 
-print("Letters: ", dict(sorted(letters.items(), key=lambda item: item[1], reverse=True)))
-print()
-print("Consonants: ", dict(sorted(cons.items(), key=lambda item: item[1], reverse=True)))
-print()
-print("Vowels: ", dict(sorted(vowels.items(), key=lambda item: item[1], reverse=True)))
-print()
-print("Cons Clusters: ", dict(sorted(cons_clusters.items(), key=lambda item: item[1], reverse=True)))
-print()
-print("Vowel Clusters: ", dict(sorted(vowel_clusters.items(), key=lambda item: item[1], reverse=True)))
+    print()
+    print(title)
+    print(ruler)
+    print(dict(sorted(dic.items(), key=lambda item: item[1], reverse=True)))
+
+print_result('Letters', letters)
+print_result('Consonants', cons)
+print_result('Vowels', vowels)
+print_result('Consonant Clusters', cons_clusters)
+print_result('Vowel Clusters', vowel_clusters)
