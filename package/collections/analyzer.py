b9b2fcccf9162c082cac057cd4507d4105e8860c
silentunion <milliner@unbc.ca>
2020-12-30 10:42:40 -0800

    Now analyzes start/mid/end vowels and consonants

--- package/collections/analyzer.py
+++ package/collections/analyzer.py
@@ -6,6 +6,9 @@ consonants_list = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
         'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
 
 letters, cons, vowels = {}, {}, {}
+start_letters, start_cons, start_vowels = {}, {}, {}
+mid_letters, mid_cons, mid_vowels = {}, {}, {}
+end_letters, end_cons, end_vowels = {}, {}, {}
 cons_clusters, vowel_clusters = {}, {}
 start_cons_clusters, mid_cons_clusters, end_cons_clusters = {}, {}, {}
 start_vow_clusters, mid_vow_clusters, end_vow_clusters = {}, {}, {}
@@ -13,6 +16,9 @@ start_vow_clusters, mid_vow_clusters, end_vow_clusters = {}, {}, {}
 letter_type, prev_letter, prev_letter_type, letter_type_list = '', '', '', ''
 
 tot_letters, tot_cons, tot_vowels = 0, 0, 0
+tot_start_letters, tot_start_cons, tot_start_vowels = 0, 0, 0
+tot_mid_letters, tot_mid_cons, tot_mid_vowels = 0, 0, 0
+tot_end_letters, tot_end_cons, tot_end_vowels = 0, 0, 0
 tot_cons_cl, tot_vowels_cl = 0, 0
 tot_start_cons_cl, tot_mid_cons_cl, tot_end_cons_cl = 0, 0, 0
 tot_start_vow_cl, tot_mid_vow_cl, tot_end_vow_cl = 0, 0, 0
@@ -73,12 +79,31 @@ for word in words_list:
                 letter_type_list += word[l]
                 if l == len(word)-1:
                     letter_type_list = dispatch_cluster(letter_type_list, letter_type, prev_letter_type, is_end=True)
+                    
+                    add_item_to_dic(word[l], end_letters)
+                    if letter_type == 'consonant':
+                        add_item_to_dic(word[l], end_cons)
+                    elif letter_type == 'vowel':
+                        add_item_to_dic(word[l], end_vowels)
+                else:
+                    
+                    add_item_to_dic(word[l], mid_letters)
+                    if letter_type == 'consonant':
+                        add_item_to_dic(word[l], mid_cons)
+                    elif letter_type == 'vowel':
+                        add_item_to_dic(word[l], mid_vowels)
             else:
                 letter_type_list = dispatch_cluster(letter_type_list, letter_type, prev_letter_type, is_beginning)
                 is_beginning = False
         else:
             letter_type_list = word[l]
             is_beginning = True
+            
+            add_item_to_dic(word[l], start_letters)
+            if letter_type == 'consonant':
+                add_item_to_dic(word[l], start_cons)
+            elif letter_type == 'vowel':
+                add_item_to_dic(word[l], start_vowels)
         
         prev_letter = word[l]
         prev_letter_type = letter_type
@@ -90,7 +115,7 @@ def remove_lower_values(dic, val):
         if v < val:
             del dic[k]
 
-remove_lower_values(cons_clusters, 20)
+remove_lower_values(cons_clusters, 1)
 remove_lower_values(vowel_clusters, 1)
 
 
@@ -104,8 +129,20 @@ def convert_to_percent(total, dic):
 convert_to_percent(tot_letters, letters)
 convert_to_percent(tot_cons, cons)
 convert_to_percent(tot_vowels, vowels)
+
+convert_to_percent(tot_start_letters, start_letters)
+convert_to_percent(tot_start_cons, start_cons)
+convert_to_percent(tot_start_vowels, start_vowels)
+convert_to_percent(tot_mid_letters, mid_letters)
+convert_to_percent(tot_mid_cons, mid_cons)
+convert_to_percent(tot_mid_vowels, mid_vowels)
+convert_to_percent(tot_end_letters, end_letters)
+convert_to_percent(tot_end_cons, end_cons)
+convert_to_percent(tot_end_vowels, end_vowels)
+
 convert_to_percent(tot_cons_cl, cons_clusters)
 convert_to_percent(tot_vowels_cl, vowel_clusters)
+
 convert_to_percent(tot_start_cons_cl, start_cons_clusters)
 convert_to_percent(tot_mid_cons_cl, mid_cons_clusters)
 convert_to_percent(tot_end_cons_cl, end_cons_clusters)
@@ -128,6 +165,15 @@ def print_result(title, dic):
 print_result('Letters', letters)
 print_result('Consonants', cons)
 print_result('Vowels', vowels)
+print_result('Start Letters', start_letters)
+print_result('Start Cons', start_cons)
+print_result('Start Vowels', start_vowels)
+print_result('Mid Letters', mid_letters)
+print_result('Mid Cons', mid_cons)
+print_result('Mid Vowels', mid_vowels)
+print_result('End Letters', end_letters)
+print_result('End Cons', end_cons)
+print_result('End Vowels', end_vowels)
 print_result('Consonant Clusters', cons_clusters)
 print_result('Vowel Clusters', vowel_clusters)
 print_result('Start Cons Clusters', start_cons_clusters)
